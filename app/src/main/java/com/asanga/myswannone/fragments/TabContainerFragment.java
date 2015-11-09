35a653083828cbb2bd7199e72e8f43fa673e903d
Asanga <asanga.lk@gmail.com>
2015-11-09 12:47:36 +1100

    Initial commit

---
+++ app/src/main/java/com/asanga/myswannone/fragments/TabContainerFragment.java
@@ -0,0 +1,8 @@
+package com.asanga.myswannone.fragments;
+
+/**
+ * Created by Asanga on 6/11/2015.
+ */
+
+public class TabContainerFragment {
+}
---
+++ app/src/main/java/com/asanga/myswannone/utils/SharedPrefsUtil.java
@@ -0,0 +1,9 @@
+package com.asanga.myswannone.utils;
+
+/**
+ * Created by Asanga on 6/11/2015.
+ */
+public class SharedPrefsUtil {
+
+
+}
